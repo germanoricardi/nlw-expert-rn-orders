80572c57f6e1902dd722daccf59508ae217e2bcc
Germano Ricardi <germanoricardi7@gmail.com>
2024-02-07 20:15:28 -0400

    Removidos arquivos não utilizados; Adicionado tailwindcss e google fonts; Criado componente de loading; Adicionado imagens de icons e splash;

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home(){
+  return (
+    <View className="pt-8 flex-1">
+      <Text className="text-white text-2xl font-heading">Hello React Native</Text>
+    </View>
+  )
+}
