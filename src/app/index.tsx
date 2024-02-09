75cb9d084ee17b12150beb16f9d467b386c14610
Germano Ricardi <germanoricardi7@gmail.com>
2024-02-08 22:44:22 -0400

    Criado flatlist das categorias; Adicionado useState para controlar categoria ativa;

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,28 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { Text, View } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function Home(){
+
+  const [category, setCategory] = useState<string>(CATEGORIES[0])
+
+  const handleCategorySelect = (category: string) => setCategory(category)
+
   return (
     <View className="pt-8 flex-1">
       <Header title="Faça seu pedido" cartQuantityItems={7} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <CategoryButton title={item} isSelected={category == item} onPress={() => handleCategorySelect(item)} />}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   )
 }
