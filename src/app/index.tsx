b31d0d9fa2398a729737ca834376b02d740c7d36
Germano Ricardi <germanoricardi7@gmail.com>
2024-02-08 23:13:00 -0400

    Criado sectionList de produtos;

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { CATEGORIES } from "@/utils/data/products";
+import { Product } from "@/components/product";
+import { CATEGORIES, MENU } from "@/utils/data/products";
 import { useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, SectionList, Text, View } from "react-native";
 
 export default function Home(){
 
@@ -22,6 +23,19 @@ export default function Home(){
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Product data={item} />
+        )}
+        renderSectionHeader={({ section: { title } }) => <Text className="text-xl text-white font-heading mt-8 mb-3">{ title }</Text>}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
       />
     </View>
   )
