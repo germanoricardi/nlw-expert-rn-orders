d701f706b84defcd56f44e472969a466436f951c
Germano Ricardi <germanoricardi7@gmail.com>
2024-02-08 22:47:30 -0400

    Adicionado borda nos botões de categorias não ativas;

--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -8,7 +8,7 @@ type CategoryButtonProps = PressableProps & {
 
 export function CategoryButton({ title, isSelected, ...rest }: CategoryButtonProps){
   return (
-    <Pressable className={clsx("bg-slate-800 px-4 justify-center rounded-md h-10", isSelected && "border-2 border-lime-300")} {...rest}>
+    <Pressable className={clsx("bg-slate-800 px-4 justify-center rounded-md h-10 border-2 border-slate-800", isSelected && "border-2 border-lime-300")} {...rest}>
       <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
     </Pressable>
   )
